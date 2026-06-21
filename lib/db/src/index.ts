2a7c36a672c72be6befec47b040daa446f1b9597
sunam0809 <haydensunam524@gmail.com>
2026-06-21 11:41:54 +0900

    fix: lib/db/src/index.ts

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,11 +1,16 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import pg from "pg";
 import * as schema from "./schema";
 
-const connectionString = process.env.DATABASE_URL;
-if (!connectionString) {
-  throw new Error("DATABASE_URL environment variable is not set");
+const { Pool } = pg;
+
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    "DATABASE_URL must be set. Did you forget to provision a database?",
+  );
 }
 
-export const db = drizzle(connectionString, { schema });
-export * from "./schema";
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = drizzle(pool, { schema });
 
+export * from "./schema";
